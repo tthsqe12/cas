54d33a8bf3db28b4b41715e3ae4e77e21f6f0eab
dan <tthsqe12@gmail.com>
2022-03-15 00:30:33 +0100

    rewroiuxcvb

--- fft_v2.cpp
+++ fft_v2.cpp
@@ -46,7 +46,7 @@ struct fftv2_ctx {
             ulong tabsize = (l == 0) ? n : n/2;
             std::vector<double> tab(tabsize);
 
-std::cout << "filling depth " << l << " for prime " << format_hex(mod.n) << std::endl;
+//std::cout << "filling depth " << l << " for prime " << format_hex(mod.n) << std::endl;
 
             ulong w = nmod_pow_ui(primitive_root, (mod.n - 1)>>l, mod);
             ulong wi = 1;
@@ -151,6 +151,47 @@ std::cout << "filling depth " << l << " for prime " << format_hex(mod.n) << std:
     void from_mpn(const ulong * a, ulong an, ulong bits);
     void point_mul(const double* b, ulong mm);
 };
+
+// pointwise mul of self with b and m
+void fftv2_ctx::point_mul(const double* b, ulong mm)
+{
+    double m = mm;
+    if (m > 0.5*p)
+        m -= p;
+
+    PD<4> M = m;
+    PD<4> n = p;
+    PD<4> ninv = pinv;
+    for (ulong I = 0; I < ulong(1) << (depth - LG_BLK_SZ); I++)
+    {
+        double* x = from_index(I);
+        const double* bx = b + offset(I);
+        ulong j = 0;
+        do {
+            PD<4> x0, x1, x2, x3, b0, b1, b2, b3;
+            x0.load(x+j+0);
+            x1.load(x+j+4);
+            x2.load(x+j+8);
+            x3.load(x+j+12);
+            b0.load(bx+j+0);
+            b1.load(bx+j+4);
+            b2.load(bx+j+8);
+            b3.load(bx+j+12);
+            x0 = mulmod2(x0, M, n, ninv);
+            x1 = mulmod2(x1, M, n, ninv);
+            x2 = mulmod2(x2, M, n, ninv);
+            x3 = mulmod2(x3, M, n, ninv);
+            x0 = mulmod2(x0, b0, n, ninv);
+            x1 = mulmod2(x1, b1, n, ninv);
+            x2 = mulmod2(x2, b2, n, ninv);
+            x3 = mulmod2(x3, b3, n, ninv);
+            x0.store(x+j+0);
+            x1.store(x+j+4);
+            x2.store(x+j+8);
+            x3.store(x+j+12);
+        } while (j += 16, j < blk_sz);
+    }
+}
 
 ulong saturate_bits(ulong a)
 {
@@ -846,67 +887,247 @@ ulong next_fft_number(ulong p)
     return (UWORD(1) << (bits)) - (UWORD(1) << (l - 1)) + 1;   
 }
 
-
+#if 0
 void fftv2_ctx::from_mpn(
     const ulong * a,
     ulong an,
     ulong bits)
 {
-    if ((bits % FLINT_BITS) != 0)
-    {
-        std::cout << "from_mpn: bad bits" << std::endl;
-        abort();
-    }
-
     ulong xn = UWORD(1) << depth;
+    ulong ttlen = bits/FLINT_BITS + 1;
+    ulong* tt = new ulong[ttlen];
+
+    FLINT_ASSERT(bits >= FLINT_BITS);
+
+    ulong mask = (UWORD(1) << (bits%FLINT_BITS)) - 1;
+
     for (ulong i = 0; i < xn; i++)
     {
-        ulong tbits = i*bits;
-        ulong toff = tbits/FLINT_BITS;
-        ulong tshift = tbits%FLINT_BITS;
+        ulong abits = i*bits;
+        ulong aoff = abits/FLINT_BITS;
+        ulong ashift = abits%FLINT_BITS;
 
-        if (tshift == 0)
+        if (ashift == 0)
         {
-            ulong r = 0;
-            if (toff + bits/FLINT_BITS < an)
-                r = mpn_mod_1(a + toff, bits/FLINT_BITS, mod.n);
-            else if (toff < an)
-                r = mpn_mod_1(a + toff, FLINT_MIN(bits/FLINT_BITS, an - toff), mod.n);
-            double rr = r;
-            if (mod.n - r < r)
-                rr -= p;
-
-if (i < 0)
-std::cout << "r[" <<  i << "]: " << rr << std::endl;
-
-            set_index(i, rr);
+            for (ulong j = 0; j < ttlen; j++)
+            {
+                tt[j] = (aoff+j < an) ? a[aoff+j] : 0;
+            }            
         }
         else
         {
-            std::cout << "from_mpn: tshift" << std::endl;
-            abort();
+            for (ulong j = 0; j < ttlen; j++)
+            {
+                ulong alo = (aoff+j+0 < an) ? a[aoff+j+0] : 0;
+                ulong ahi = (aoff+j+1 < an) ? a[aoff+j+1] : 0;
+                tt[j] = (alo >> ashift) | (ahi << (FLINT_BITS - ashift));
+            }
         }
+
+        tt[ttlen-1] &= mask;
+
+
+
+        ulong r = mpn_mod_1(tt, ttlen, mod.n);
+//std::cout << "x[" << i << "]: " << r << "    tt[0]: " << format_hex(tt[0]) << "  tt[1]: " << format_hex(tt[1]) << std::endl;
+        set_index(i, (mod.n - r < r) ? -double(mod.n-r) : double(r));
     }
 }
+#endif
 
-// pointwise mul of self with b and m
-void fftv2_ctx::point_mul(const double* b, ulong mm)
+#if 0
+void fftv2_ctx::from_mpn(
+    const ulong * a,
+    ulong an,
+    ulong bits)
 {
-    double m = mm;
-    if (m > 0.5*p)
-        m -= p;
+    ulong xn = UWORD(1) << depth;
+    ulong ttlen = bits/FLINT_BITS + 1;
+    ulong* tt = new ulong[ttlen];
 
-    for (ulong I = 0; I < ulong(1) << (depth - LG_BLK_SZ); I++)
+    FLINT_ASSERT(bits >= FLINT_BITS);
+
+    ulong mask = (UWORD(1) << (bits%FLINT_BITS)) - 1;
+
+    ulong* two_pow = new ulong[ttlen*FLINT_BITS];
+
+    two_pow[0] = 1;
+    for (ulong i = 1; i < ttlen*FLINT_BITS; i++)
+        two_pow[i] = nmod_add(two_pow[i-1], two_pow[i-1], mod);
+
+#define aindex(i) (((i) < an) ? a[i] : UWORD(0))
+
+#define MADD(hi, lo, a, b) \
+{ \
+    ulong _a = a; \
+    ulong _b = b; \
+    ulong _phi, _plo; \
+    umul_ppmm(_phi, _plo, _a, _b); \
+    add_ssaaaa(hi, lo, hi, lo, _phi, _plo); \
+}
+
+    for (ulong i = 0; i < xn; i++)
     {
-        double* x = from_index(I);
-        const double* bx = b + offset(I);
-        for (ulong j = 0; j < blk_sz; j++)
+        ulong abits = i*bits;
+        ulong aoff = abits/FLINT_BITS;
+        ulong ashift = abits%FLINT_BITS;
+        ulong j, c, r, sum_lo, sum_hi;
+
+        sum_lo = sum_hi = 0;
+        if (ashift == 0)
         {
-            x[j] = mulmod2(x[j], bx[j], p, pinv);
-            x[j] = mulmod2(x[j], m, p, pinv);
+            for (j = 0; j < ttlen - 1; j++)
+                MADD(sum_hi, sum_lo, aindex(aoff+j), two_pow[FLINT_BITS*j]);
+            MADD(sum_hi, sum_lo, mask & aindex(aoff+j), two_pow[FLINT_BITS*(ttlen - 1)]);
+        }
+        else
+        {
+            for (j = 0; j < ttlen - 1; j++)
+            {
+                c = (aindex(aoff+j) >> ashift) | (aindex(aoff+j+1) << (FLINT_BITS - ashift));
+                MADD(sum_hi, sum_lo, c, two_pow[FLINT_BITS*j]);
+            }
+            c = (aindex(aoff+j) >> ashift) | (aindex(aoff+j+1) << (FLINT_BITS - ashift));
+            MADD(sum_hi, sum_lo, mask & c, two_pow[FLINT_BITS*j]);
+        }
+
+        r = sum_hi;
+        sum_hi = 0;
+        MADD(sum_hi, sum_lo, r, two_pow[FLINT_BITS]);
+        NMOD_RED2(r, sum_hi, sum_lo, mod);
+
+//std::cout << "x[" << i << "]: " << r /* << "    tt[0]: " << format_hex(tt[0]) << "  tt[1]: " << format_hex(tt[1])*/ << std::endl;
+        set_index(i, (mod.n - r < r) ? -double(mod.n-r) : double(r));
+    }
+
+    delete two_pow;
+}
+#endif
+
+
+// TODO parameterize by bits and number of primes
+void fftv2_ctx::from_mpn(
+    const ulong * a_,
+    ulong an_,
+    ulong bits)
+{
+    bits = 71;
+    FLINT_ASSERT(bits >= FLINT_BITS);
+
+    const uint32_t* a = reinterpret_cast<const uint32_t*>(a_);
+    ulong an = 2*an_;
+    ulong xn = UWORD(1) << depth;
+    ulong ttlen = bits + 64;
+
+    double* two_pow = new double[ttlen];
+
+    two_pow[0] = 1;
+    ulong tp = 1;
+    for (ulong i = 1; i < ttlen; i++)
+    {
+        tp = nmod_add(tp, tp, mod);
+        two_pow[i] = (mod.n - tp < tp) ? -double(mod.n-tp) : double(tp);
+    }
+
+    ulong masks[32];
+    masks[0] = 0x00000000;
+    for (ulong i = 1; i < 32; i++)
+        masks[i] = 2*masks[i-1]+1;
+
+    // if i*bits + 32 < 32*an, then aindex is easy
+    ulong end_easy = std::min(xn, (32*an - 33)/bits);
+
+    // if i*bits >= 32*an, then aindex is zero
+    ulong end_hard = std::min(xn, (32*an + bits - 1)/bits);
+
+    ulong i = 0;
+
+#define CODE(ir) \
+    { \
+        ulong k = ((i+ir)*bits)/32; \
+        ulong j = ((  ir)*bits)%32; \
+        double x = double(a[k] >> j); \
+        k++; \
+        j = 32 - j; \
+        while (j + 32 <= bits) \
+        { \
+            x += mulmod2(double(a[k]), two_pow[j], p, pinv); \
+            k++; \
+            j += 32; \
+        } \
+        assert(j <= bits < j + 32); \
+        x += mulmod2(double(a[k] & masks[bits - j]), two_pow[j], p, pinv); \
+        set_index(i+ir, reduce_to_pm1n(x, p, pinv)); \
+    }
+
+    if ((bits % 4) == 0)
+    {
+        end_easy &= -uint32_t(8);
+        for ( ; i < end_easy; i += 8)
+        {
+            CODE(0);CODE(1);CODE(2);CODE(3);
+            CODE(4);CODE(5);CODE(6);CODE(7);
         }
     }
+    else if ((bits % 2) == 0)
+    {
+        end_easy &= -uint32_t(16);
+        for ( ; i < end_easy; i += 16)
+        {
+            CODE(0);CODE(1);CODE(2);CODE(3);
+            CODE(4);CODE(5);CODE(6);CODE(7);
+            CODE(8);CODE(9);CODE(10);CODE(11);
+            CODE(12);CODE(13);CODE(14);CODE(15);
+        }
+    }
+    else
+    {
+        end_easy &= -uint32_t(32);
+        for ( ; i < end_easy; i += 32)
+        {
+            CODE(0);CODE(1);CODE(2);CODE(3);
+            CODE(4);CODE(5);CODE(6);CODE(7);
+            CODE(8);CODE(9);CODE(10);CODE(11);
+            CODE(12);CODE(13);CODE(14);CODE(15);
+            CODE(16);CODE(17);CODE(18);CODE(19);
+            CODE(20);CODE(21);CODE(22);CODE(23);
+            CODE(24);CODE(25);CODE(26);CODE(27);
+            CODE(28);CODE(29);CODE(30);CODE(31);
+        }
+    }
+#undef CODE
+
+    for (; i < end_hard; i++)
+    {
+        ulong k = (i*bits)/32;
+        ulong j = (i*bits)%32;
+
+#define aindex(i) (((i) < an) ? a[i] : UWORD(0))
+
+        double x = double(aindex(k) >> j);
+        k++;
+        j = 32 - j;
+        while (j + 32 <= bits)
+        {
+            x += mulmod2(double(aindex(k)), two_pow[j], p, pinv);
+            k++;
+            j += 32;
+        }
+        x += mulmod2(double(aindex(k) & masks[bits - j]), two_pow[j], p, pinv);
+
+#undef aindex
+
+        set_index(i, reduce_to_pm1n(x, p, pinv));
+    }
+
+    for (; i < xn; i++)
+    {
+        set_index(i, 0.0);
+    }
+
+    delete two_pow;
 }
+
 
 
 struct crt_data {
@@ -964,10 +1185,15 @@ struct mpn_ctx_v2 {
     {
         ffts.emplace_back(p);
         ulong len = crts.back().coeff_len;
-        ulong* t = new ulong[len + 1];
+        ulong* t = new ulong[len + 2];
 
+        t[len + 1] = 0;
         t[len] = mpn_mul_1(t, crts.back().prod_primes(), len, p);
         len += (t[len] != 0);
+
+        // leave room for one more bit
+        if (FLINT_SIGN_EXT(t[len-1]) != 0)
+            len += 1;
 
         crts.emplace_back(p, len, nprimes());
 
@@ -1002,7 +1228,7 @@ void mpn_mul_v2(
     const ulong* b, ulong bn)
 {
     ulong zn = an + bn;
-    ulong bits = 64;
+    ulong bits = 71;
     ulong alen = cld(FLINT_BITS * an, bits);
     ulong blen = cld(FLINT_BITS * bn, bits);
     alen = FLINT_MAX(1, alen);
@@ -1024,13 +1250,14 @@ timeit_t timer;
     double* bbuf = new double[Q.ffts[0].data_size()];
     ulong* zbuf = new ulong[zlen*zcoeff_len];
 
-
 std::cout << "depth: " << depth << std::endl;
+std::cout << " alen: " << alen << std::endl;
+std::cout << " blen: " << blen << std::endl;
 
     for (ulong i = 0; i < np; i++)
     {
         fftv2_ctx& ctx = Q.ffts[i];
-//std::cout << "doing prime: " << ctx.p << std::endl;
+std::cout << "doing prime " << i << ": " << format_hex(ctx.mod.n) << std::endl;
         ctx.set_depth(depth);
 
         ctx.set_data(bbuf);
@@ -1062,7 +1289,6 @@ timeit_start(timer);
             thi = cop >> (FLINT_BITS - depth);
             tlo = cop << (depth);
             NMOD_RED2(t1, thi, tlo, ctx.mod);
-//            t1 = ulong(1) << depth;
             ctx.point_mul(bbuf, nmod_inv(t1, ctx.mod));
         }
 timeit_stop(timer);
@@ -1085,12 +1311,6 @@ timeit_start(timer);
                 y = ctx.mod.n + xx;
             else
                 y = xx;
-
-if (j < 0)
-{
-    std::cout << "y[" << j << "]: " << y << std::endl;
-}
-
             if (i == 0)
                 mpn_mul_1(zbuf + j*zcoeff_len, mult, zcoeff_len, y);
             else
@@ -1121,18 +1341,10 @@ timeit_start(timer);
 
         if (toff >= zn)
             break;
-/*
-std::cout << "i: " << i << ", toff: " << toff << ", tshift: " << tshift << ", adding length: " << FLINT_MIN(zcoeff_len, zn - toff) << "  ";
-std::cout << "t: " << format_hex(t[0]) << ", " <<
-                      format_hex(t[1]) << ", " <<
-                      format_hex(t[2]) << ", " <<
-                      format_hex(t[3]) << std::endl;
-*/
 
         if (tshift == 0)
         {
             mpn_add_n(z + toff, z + toff, t, FLINT_MIN(zcoeff_len, zn - toff));
-//std::cout << "z[" << toff << "]: " << format_hex(z[toff]) << std::endl;
         }
         else
         {
@@ -1152,6 +1364,14 @@ std::cout << "     final: " << timer->wall << std::endl;
 }
 
 
+/*
+std::cout << "i: " << i << ", toff: " << toff << ", tshift: " << tshift << ", adding length: " << FLINT_MIN(zcoeff_len, zn - toff) << "  ";
+std::cout << "t: " << format_hex(t[0]) << ", " <<
+                      format_hex(t[1]) << ", " <<
+                      format_hex(t[2]) << ", " <<
+                      format_hex(t[3]) << std::endl;
+*/
+
 void test_mpn_mul_v2(mpn_ctx_v2& Q, ulong an, ulong bn)
 {
     ulong* a = new ulong[an];
@@ -1162,10 +1382,10 @@ void test_mpn_mul_v2(mpn_ctx_v2& Q, ulong an, ulong bn)
     ulong new_time, old_time;
 
     for (ulong i = 0; i < an; i++)
-        a[i] = -UWORD(1);
+        a[i] = -UWORD(3);
 
     for (ulong i = 0; i < bn; i++)
-        b[i] = -UWORD(1);
+        b[i] = -UWORD(2);
 
 timeit_start(timer);
     if (an < bn)
